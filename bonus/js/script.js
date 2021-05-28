ba4af6a603dc5dc8a1c8fc1a2ddd1fe5280b112e
MarcoDeRosso <mcoder@hotmail.it>
2021-05-28 15:36:04 +0200

    age calc

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,16 +1,18 @@
 var distance = prompt("Quanti chilomentri devi percorrere?")
 
-var year = prompt("Quale è il tuo anno di nascita?")
+var anno = prompt("Quale è il tuo anno di nascita?")
+
 var d = new Date();
-var year = d.getFullYear() - parseInt(age);
+
+var age = d.getFullYear() - parseInt(anno);
 
 var ticket = parseInt(distance) * 0.21;
 
 var tiketElement = document.getElementById("ticketGenerator");
 
-if (year < 18) {
+if (age < 18) {
     tiketElement.innerHTML += "Il prezzo del suo biglietto è: " + ((ticket * 20) / 100).toFixed(2) + "€";
-} else if (year >= 65) {
+} else if (age >= 65) {
     tiketElement.innerHTML += "Il prezzo del suo biglietto è: " + ((ticket * 40) / 100).toFixed(2) + "€";
 } else {
     tiketElement.innerHTML += "Il prezzo del suo biglietto è: " + ticket.toFixed(2) + "€";
