013cad6688a2077bac89ab46708073dc4cd7466a
MarcoDeRosso <mcoder@hotmail.it>
2021-05-31 10:17:01 +0200

    refactoring

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -11,9 +11,11 @@ var ticket = parseInt(distance) * 0.21;
 var tiketElement = document.getElementById("ticketGenerator");
 
 if (age < 18) {
-    tiketElement.innerHTML += "Il prezzo del suo biglietto è: " + ((ticket * 20) / 100).toFixed(2) + "€";
+    ticket -= ticket * 20 / 100;
 } else if (age >= 65) {
-    tiketElement.innerHTML += "Il prezzo del suo biglietto è: " + ((ticket * 40) / 100).toFixed(2) + "€";
-} else {
-    tiketElement.innerHTML += "Il prezzo del suo biglietto è: " + ticket.toFixed(2) + "€";
+    ticket -= ticket * 40 / 100;
 }
+
+ticket = ticket.toFixed(2);
+
+tiketElement.innerHTML += "Il prezzo del suo biglietto è: " + ticket;
